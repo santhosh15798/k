811fe95f304996f7f80e7f46afc3a9569e173454
santhosh15798 <53962019+santhosh15798@users.noreply.github.com>
2019-08-10 10:50:20 +0530

    Update and rename oe.py to olu.py

--- oe.py
+++
@@ -1,7 +0,0 @@
-i=int(input())
-if(i>0):
-	print("Positive")
-elif(i==0):
-	print("Zero")
-else:
-	print("Negative")
---
+++ olu.py
@@ -0,0 +1,7 @@
+r =int(input())
+if(r<1):
+	print("invalid")
+elif (r%2==0):
+	print("Even")
+else:
+	print("Odd")
